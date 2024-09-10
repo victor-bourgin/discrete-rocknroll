10a0127b008edb130cfe0a7c4b5f27db65a4c888
TheSquareRoot <victor.bourgin1@gmail.com>
2024-09-10 09:59:09 +0200

    Added tracking of simulation output quantities.

--- rnr/config.py
+++ rnr/config.py
@@ -7,11 +7,11 @@ def setup_logging(name, log_file):
     logger.setLevel(logging.DEBUG)
 
     # Create handlers
-    file_handler = logging.FileHandler(log_file, mode='w')
+    file_handler = logging.FileHandler(log_file, mode='a')
     console_handler = logging.StreamHandler()
 
     file_handler.setLevel(logging.DEBUG)
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(logging.DEBUG)
 
     # Add handlers to logger
     logger.addHandler(file_handler)
--- rnr/simulation.py
+++ rnr/simulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from time import time
 
+from typing import List, Tuple
+from numpy.typing import NDArray
 from .distribution import Distribution
 from .flow import Flow
 
@@ -21,11 +23,17 @@ class Simulation:
         self.flow = flow
         self.current_time = 0
 
-    def run(self, duration: float, dt: float,) -> None:
-
+    def run(self, duration: float, dt: float,) -> Tuple[NDArray, NDArray, NDArray]:
+        """
+        Advances the simulation by the given time, starting from the current simulation time.
+        """
         # Initialize arrays
         time_array = np.arange(self.current_time, duration, dt)
         nsteps = len(time_array)
+
+        # Output quantities
+        total_parts = np.zeros_like(time_array)
+        instant_rate = np.zeros_like(time_array)
 
         self._logger.info("Starting simulation...")
         tstart = time()
@@ -34,6 +42,11 @@ class Simulation:
             change = rate_binned(self.distribution, self.flow, dt)
             change = np.where(change > self.distribution.count, self.distribution.count, change)
 
+            total_parts[i] = self.distribution.partnumber
+            instant_rate[i] = np.sum(change)
+
             self.distribution.count = self.distribution.count - change
 
         self._logger.info(f"Finished simulation in {time() - tstart:.2f}s")
+
+        return time_array, total_parts, instant_rate
