7d0cbbb638d442ea1ada15578b7aa9e7570571ec
TheSquareRoot <victor.bourgin1@gmail.com>
2024-09-13 23:28:51 +0200

    Moved run script to core.py

--- rnr/core.py
+++ rnr/core.py
@@ -1,15 +1,50 @@
-from .distribution import DistributionBuilder
+import matplotlib.pyplot as plt
+import toml
 
-def main():
-    # PARAMETERS
-    nparts = 1000
-    nbins = 10
-    fmin = 0.0
-    fmax = 0.5
+from rnr.distribution import DistributionBuilder
+from rnr.flow import Flow
+from rnr.simulation import Simulation
 
-    distrib = DistributionBuilder(nparts, nbins, fmin, fmax).distribution
+# TODO: Add plotting functionnalities
 
-    distrib.plot()
 
-if __name__ == '__main__':
-    main()
+def single_run():
+    # Load config file
+    with open("config.toml", "r") as f:
+        config = toml.load(f)
+
+    # DISTRIBUTION PARAMETERS
+    nparts = int(config["distribution"]["nparts"])
+    radius = config["distribution"]["radius"]
+    nbins = config["distribution"]["nbins"]
+    fmin = config["distribution"]["fmin"]
+    fmax = config["distribution"]["fmax"]
+
+    # FLOW PARAMETERS
+    friction_vel = config["flow"]["friction_vel"]
+    fluid_density = config["flow"]["fluid_density"]
+    kin_visco = config["flow"]["kin_visco"]
+    surf_energy = config["flow"]["surf_energy"]
+
+    # SIMULATION PARAMETERS
+    duration = config["simulation"]["duration"]
+    dt = config["simulation"]["dt"]
+
+    # Initialize adhesion distribution
+    builder = DistributionBuilder(radius, nparts, nbins, fmin, fmax)
+    distrib = builder.generate()
+
+    # Plot initial distribution
+    distrib.plot(scale='log')
+    plt.savefig("./figs/initial_distrib.pdf", dpi=300)
+
+    # Initialize the flow
+    flow = Flow(friction_vel, fluid_density, kin_visco, surf_energy)
+
+    # Instanciate simulation and run it
+    sim = Simulation(distrib, flow)
+    time, total_parts, instant_rate = sim.run(duration, dt)
+
+
+def validation():
+    pass
